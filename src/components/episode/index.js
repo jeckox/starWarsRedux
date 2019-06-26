18fb79e18d26b909d0728ba8cf17f28da83ebcd4
User <user@rba-hackerrank.com>
2019-06-26 17:09:34 +0000

    Auto commit on Run Tests
    Commit by HackerRank

---
+++ src/components/episode/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const episode = () =>{
+    return (
+        <div>
+            <h2>Nombre del episodio </h2>
+            <p>Episodio</p>
+            <p>Director</p>
+            <ul>
+                <li>Personaje 1</li>
+            </ul>
+        </div>
+    );   
+}
